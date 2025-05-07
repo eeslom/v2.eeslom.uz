0ac5784f308cda5992672227558e8eea241258b0
Islom Murodov <islomurodov@icloud.com>
2025-05-08 01:03:40 +0500

    fix: github username

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,4 +1,4 @@
 export const appName = 'Islom Murodov'
 export const appDescription = 'Hi I\'m a Former Frontend Developer and JavaScript Engineer'
 export const appCreator = 'Islom Murodov'
-export const githubUsername = 'islomurodov'
+export const githubUsername = 'eeslom'
