9fc4ff4d82a2fcc80298b36c45ce499ffb3120a5
Islom Murodov <brilliantpul@gmail.com>
2026-01-08 06:02:11 +0500

    Refactor GitHub repos API call with error handling
    Updated the API URL for fetching GitHub repositories and added error handling.

--- server/api/github/repos.ts
+++ server/api/github/repos.ts
@@ -4,11 +4,14 @@ import { githubUsername } from '~/constants/index'
 export default defineEventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig(event)
 
-  const data = await $fetch<Promise<GithubApiRepoResponse[]>>(`https://api.github.com/users/${githubUsername}/repos`, {
-    headers: {
-      Authorization: `Bearer: ${runtimeConfig.github.TOKEN}`,
-    },
-  })
-
-  return data
+  try {
+    const data = await $fetch<Promise<GithubApiRepoResponse[]>>(`${runtimeConfig.github.API_URL}/users/${githubUsername}/repos`, {
+      headers: {
+        Authorization: `Bearer: ${runtimeConfig.github.TOKEN}`,
+      },
+    })
+  
+    return data
+  }
+  catch (error) { return error }
 })
