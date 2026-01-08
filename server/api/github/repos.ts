3a0c98a8281939428d07c9a5a1f6464252507e22
Islom Murodov <brilliantpul@gmail.com>
2026-01-08 05:06:33 +0500

    Update GitHub API URL to use direct endpoint

--- server/api/github/repos.ts
+++ server/api/github/repos.ts
@@ -2,9 +2,8 @@ import type { GithubApiRepoResponse } from '~/types/GithubApi'
 import { githubUsername } from '~/constants/index'
 
 export default defineEventHandler(async (event) => {
-  const { GITHUB_API_URL } = useRuntimeConfig(event)
 
-  const data = await $fetch<Promise<GithubApiRepoResponse[]>>(`${GITHUB_API_URL}/users/${githubUsername}/repos`)
+  const data = await $fetch<Promise<GithubApiRepoResponse[]>>(`https://api.github.com/users/${githubUsername}/repos`)
 
   return data
 })
